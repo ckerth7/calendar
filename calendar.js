59546a2317c0b11aa3d6d617e8869897d28561e2
ckerth7 <36397666+ckerth7@users.noreply.github.com>
2024-01-02 20:23:38 +0100

    Validation of the calendar removed.

--- calendar.js
+++ calendar.js
@@ -1,16 +1,5 @@
 
 function toggleVisibility(day, number) {
-
-    const date = new Date();
-    if (date.getMonth() != 11) {
-        alert('Es muss Dezember sein.');
-        return;
-    }
-    if (day > date.getDate()) {
-        alert('Zu früh :)');
-        return;
-    }
-
     if (number == 2) {
         document.getElementById(day + '_1').classList.remove('hidden');
         document.getElementById(day + '_1').classList.add('visible');
